84830a8ac12e2cc081cee560d9336722bf528f7f
mdilauro39 <mdilauro39@gmail.com>
2019-01-24 20:05:58 -0300

    mi primera app en django, segundo commit

--- alumnos/settings.py
+++ alumnos/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6(*b0*)x$3ou$2hh^vrv%y(zgno5ajs@hppdrpy5f@1hwci&o&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', ' . pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
